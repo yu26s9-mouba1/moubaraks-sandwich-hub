8dc67da577b8ce4cd0b26c396236ad4a3ef0cd21
Moubarak Congacou <cmouba1999@gmail.com>
2026-05-23 18:53:13 -0400

    Built the Chips class and added the override methods that calcultes calcultes the chips price

--- src/main/java/com/pluralsight/models/Chips.java
+++ src/main/java/com/pluralsight/models/Chips.java
@@ -1,4 +1,38 @@
 package com.pluralsight.models;
 
-public class Chips {
+
+public class Chips extends Product {
+
+    private String chipType;
+
+
+    //Constructor
+    public Chips(String chipType) {
+        this.chipType = chipType;
+    }
+
+    //Getter and setter
+    public String getChipType() {
+        return chipType;
+    }
+
+    public void setChipType(String chipType) {
+        this.chipType = chipType;
+    }
+
+
+    //Override methods to get the appropriate chip calculations
+    @Override
+    //returns 1.50 for any type of chip
+    public double getPrice() {
+        return 1.50;
+
+    }
+
+    @Override
+    public String getDetails() {
+        return "chipType : " + chipType ;
+    }
+
+
 }
