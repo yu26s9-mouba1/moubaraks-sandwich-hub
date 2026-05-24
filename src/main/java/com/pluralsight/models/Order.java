da2f3316d58f149236be5ba71e396b5ee1c1c82f
Moubarak Congacou <cmouba1999@gmail.com>
2026-05-23 23:12:00 -0400

    Built the Order class that stores, adds products then calculates and generate the receipt details

--- src/main/java/com/pluralsight/models/Order.java
+++ src/main/java/com/pluralsight/models/Order.java
@@ -1,4 +1,48 @@
 package com.pluralsight.models;
+import  java.util.ArrayList;
+
 
 public class Order {
+
+    private ArrayList<Product> items;
+
+    public Order(ArrayList<Product> items) {
+        this.items = items;
+    }
+
+    /**
+     * Adds product into an order
+     */
+   public void addItem(Product item) {
+      items.add(item);
+   }
+
+    /**
+     * Calculates the entire and return the total price
+     */
+    public double getTotal() {
+        double total = 0;
+        for (Product item : items) {
+            total += item.getPrice();
+        }
+        return total;
+
+    }
+
+    /**
+     * Creates the menu order summary text (details of the order)
+     */
+    public String getReceipt() {
+        String details = "";
+        for (Product item : items) {
+           details += item.getDetails() + "\n";
+        }
+        details += "Total Price: " + getTotal() ;
+        return details;
+
+    }
+
+
+
+
 }
