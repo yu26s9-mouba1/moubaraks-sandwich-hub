89a36171c54c403b6252e7c99e187260b89e1366
Moubarak Congacou <cmouba1999@gmail.com>
2026-05-23 21:57:50 -0400

    Built the Topping class with apropriate data, getters and setters

--- src/main/java/com/pluralsight/models/Topping.java
+++ src/main/java/com/pluralsight/models/Topping.java
@@ -1,4 +1,61 @@
 package com.pluralsight.models;
 
 public class Topping {
+
+    private String name;
+    private String type;
+    private boolean extra;
+
+    //Constructors
+    public Topping(String name, String type, boolean extra) {
+        this.name = name;
+        this.type = type;
+        this.extra = extra;
+    }
+
+
+    //Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isExtra() {
+        return extra;
+    }
+
+    public void setExtra(boolean extra) {
+        this.extra = extra;
+    }
+
+
+    //Calculates the price after toppings
+//    public double getPrice() {
+//        double price = 0;
+//        if (type.equalsIgnoreCase("meat")) {
+//            if (isExtra()){
+//                price =
+//
+//            }
+
+
+
+
+
+
+
+
+
+
 }
