110c1a1806049c8a9d364153140b916a8e75debb
Moubarak Congacou <cmouba1999@gmail.com>
2026-05-26 01:55:58 -0400

    Created a signature sandwich class BLT and added new signature objects

---
+++ src/main/java/com/pluralsight/models/BLT.java
@@ -0,0 +1,29 @@
+package com.pluralsight.models;
+
+public class BLT extends Sandwich {
+
+
+    //Constructor and super
+    public BLT(int sandwichSize, boolean isToasted, String bread) {
+        super(8, true, "white");
+
+
+        //New BLT Topping objects to be added to the order menu 
+        Topping bacon = new Topping("Bacon", "Meat", false);
+        addTopping(bacon);
+
+        Topping cheddar = new Topping("Cheddar", "cheese", false);
+        addTopping(cheddar);
+
+        Topping lettuce = new Topping("Lettuce", "Regular", false);
+        addTopping(lettuce);
+
+        Topping tomato = new Topping("Tomato", "Regular", false);
+        addTopping(tomato);
+
+        Topping ranch = new Topping("Ranch", "Regular", false);
+        addTopping(ranch);
+
+
+    }
+}
